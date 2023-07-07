d64dd1f28dc9b19083d861b8b0e25342aed94825
Breno Farias <xxdestructionxx1900@gmail.com>
2023-07-07 03:04:01 -0300

    Adicionar arquivos de projeto.

---
+++ Capitulo12/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capitulo12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void italicCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BoldCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ExercicioCap12/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercicioCap12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BoldCheckBox_CheckedChanged(object sender, EventArgs e) // bold
+        {
+            outputLabel.Font = new Font(outputLabel.Font, outputLabel.Font.Style ^ FontStyle.Bold);
+        }
+
+        private void italicCheckBox_CheckedChanged(object sender, EventArgs e) // italic
+        {
+            outputLabel.Font = new Font(outputLabel.Font, outputLabel.Font.Style ^ FontStyle.Italic);
+        }
+        private void underlineCheckBox_CheckedChanged_1(object sender, EventArgs e) // underline
+        {
+            outputLabel.Font = new Font(outputLabel.Font, outputLabel.Font.Style ^ FontStyle.Underline);
+        }
+        private void strikeoutCheckBox_CheckedChanged(object sender, EventArgs e) // strikeout
+        {
+            outputLabel.Font = new Font(outputLabel.Font, outputLabel.Font.Style ^ FontStyle.Strikeout);
+        }
+
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
