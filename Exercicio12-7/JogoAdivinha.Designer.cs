57c54e7a82d8cfa43343e426a038acbb6752d55b
Breno Farias <xxdestructionxx1900@gmail.com>
2023-07-07 14:55:46 -0300

    Finalizado

---
+++ Exercicio12-7/JogoAdivinha.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Exercicio12_7
+{
+    partial class JogoAdivinha
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jogarNovamenteButton = new System.Windows.Forms.Button();
+            this.resultadoLabel = new System.Windows.Forms.Label();
+            this.palpiteTextBox = new System.Windows.Forms.TextBox();
+            this.verificarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jogarNovamenteButton
+            // 
+            this.jogarNovamenteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jogarNovamenteButton.Location = new System.Drawing.Point(643, 303);
+            this.jogarNovamenteButton.Name = "jogarNovamenteButton";
+            this.jogarNovamenteButton.Size = new System.Drawing.Size(114, 31);
+            this.jogarNovamenteButton.TabIndex = 0;
+            this.jogarNovamenteButton.Text = "Jogar Novamente";
+            this.jogarNovamenteButton.UseVisualStyleBackColor = true;
+            this.jogarNovamenteButton.Visible = false;
+            this.jogarNovamenteButton.Click += new System.EventHandler(this.jogarNovamenteButton_Click);
+            // 
+            // resultadoLabel
+            // 
+            this.resultadoLabel.AutoSize = true;
+            this.resultadoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultadoLabel.Location = new System.Drawing.Point(162, 113);
+            this.resultadoLabel.Name = "resultadoLabel";
+            this.resultadoLabel.Size = new System.Drawing.Size(433, 13);
+            this.resultadoLabel.TabIndex = 1;
+            this.resultadoLabel.Text = "Eu pensei em um número entre 1 e 1000. Você consegue adivinhar qual é?";
+            // 
+            // palpiteTextBox
+            // 
+            this.palpiteTextBox.Location = new System.Drawing.Point(266, 141);
+            this.palpiteTextBox.Name = "palpiteTextBox";
+            this.palpiteTextBox.Size = new System.Drawing.Size(217, 20);
+            this.palpiteTextBox.TabIndex = 2;
+            // 
+            // verificarButton
+            // 
+            this.verificarButton.Cursor = System.Windows.Forms.Cursors.Help;
+            this.verificarButton.Location = new System.Drawing.Point(105, 199);
+            this.verificarButton.Name = "verificarButton";
+            this.verificarButton.Size = new System.Drawing.Size(128, 23);
+            this.verificarButton.TabIndex = 3;
+            this.verificarButton.Text = "Verificar Resposta";
+            this.verificarButton.UseVisualStyleBackColor = true;
+            this.verificarButton.Click += new System.EventHandler(this.verificarButton_Click);
+            // 
+            // JogoAdivinha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.verificarButton);
+            this.Controls.Add(this.palpiteTextBox);
+            this.Controls.Add(this.resultadoLabel);
+            this.Controls.Add(this.jogarNovamenteButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "JogoAdivinha";
+            this.Text = "ADIVINHE O NÚMERO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button jogarNovamenteButton;
+        private System.Windows.Forms.Label resultadoLabel;
+        private System.Windows.Forms.TextBox palpiteTextBox;
+        private System.Windows.Forms.Button verificarButton;
+    }
+}
+
---
+++ Exercicio12-7/JogoAdivinha.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio12_7
+{
+    public partial class JogoAdivinha : Form
+    {
+        private Random random = new Random();
+        private int numeroAleatorio;
+        private int tentativas;
+        private Color corPadrao;
+
+        public JogoAdivinha()
+        {
+            InitializeComponent();
+            ReiniciarJogo();
+            corPadrao = this.BackColor;
+        }
+
+        private void ReiniciarJogo()
+        {
+            numeroAleatorio = random.Next(1, 1001);
+            tentativas = 0;
+            resultadoLabel.Text = "Eu pensei em um número entre 1 e 1000. Você consegue adivinhar qual é?";
+            palpiteTextBox.Text = "";
+            this.BackColor = corPadrao;
+            palpiteTextBox.Enabled = true;
+            verificarButton.Enabled = true;
+            jogarNovamenteButton.Enabled = false;
+            jogarNovamenteButton.Visible = false;
+        }
+
+        private void VerificarPalpite()
+        {
+            string entrada = palpiteTextBox.Text;
+            int palpite;
+
+            if (!int.TryParse(entrada, out palpite))
+            {
+                MessageBox.Show("Por favor, digite um número válido.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            tentativas++;
+
+            if (palpite < numeroAleatorio)
+            {
+                resultadoLabel.Text = "Pequeno demais!";
+                this.BackColor = Color.Blue;
+            }
+            else if (palpite > numeroAleatorio)
+            {
+                resultadoLabel.Text = "Grande demais!";
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                resultadoLabel.Text = "Correto!";
+                this.BackColor = Color.Green;
+                palpiteTextBox.Enabled = false;
+                verificarButton.Enabled = false;
+                jogarNovamenteButton.Enabled = true;
+                jogarNovamenteButton.Visible = true;
+            }
+
+            palpiteTextBox.Text = "";
+        }
+
+        private void verificarButton_Click(object sender, EventArgs e)
+        {
+            VerificarPalpite();
+        }
+
+        private void jogarNovamenteButton_Click(object sender, EventArgs e)
+        {
+            ReiniciarJogo();
+        }
+    }
+}
