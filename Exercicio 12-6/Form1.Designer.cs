07050d7282235199d8e44079d64d8ec37192a291
Breno Farias <xxdestructionxx1900@gmail.com>
2023-07-07 12:09:47 -0300

    atualizado

--- Exercicio 12-6/Form1.Designer.cs
+++ Exercicio 12-6/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnPreto = new System.Windows.Forms.RadioButton();
             this.btnVerde = new System.Windows.Forms.RadioButton();
@@ -191,8 +192,9 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Painter";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
