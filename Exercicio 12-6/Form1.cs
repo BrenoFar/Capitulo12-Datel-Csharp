5aed23507ea7a6e8e8a72e722096315f663fb338
Breno Farias <xxdestructionxx1900@gmail.com>
2023-07-07 05:48:03 -0300

    Atualizado

---
+++ Exercicio 12-6/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio_12_6
+{
+    public partial class Form1 : Form
+    {
+        bool shouldPaint = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
